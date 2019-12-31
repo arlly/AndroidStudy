d7824501e881379d10138975ab521e209d77d09c
FuminoriArimoto <arimoto@queserser.co.jp>
2019-12-31 17:57:11 +0900

    Webビューを実装した。

--- app/src/main/java/com/example/mybasicapplication/MainActivity.java
+++ app/src/main/java/com/example/mybasicapplication/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.mybasicapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -15,7 +21,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private final static String TAG_MESSAGE = "0";
     private final static String TAG_YESNO = "1";
     private final static String TAG_TEXT = "2";
-    private final static String TAG_IMAGE = "3";
+    private final static String TAG_WEB = "3";
 
 
     @Override
@@ -31,10 +37,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         layout.addView(makeButton("メッセージダイアログの表示", TAG_MESSAGE));
         layout.addView(makeButton("YesNoダイアログの表示", TAG_YESNO));
         layout.addView(makeButton("テキスト入力ダイアログの表示", TAG_TEXT));
-
-
+        layout.addView(makeButton("WEBページの表示", TAG_WEB));
     }
 
+    /**
+     *
+     * @param text
+     * @param tag
+     * @return
+     */
     private Button makeButton(String text, String tag) {
         Button button = new Button(this);
         button.setText(text);
@@ -50,11 +61,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void changeLabel(View view) {
         TextView tv = (TextView) findViewById(R.id.chage);
         tv.setText("ありまろ！！");
-
     }
 
+
+
+    /**
+     *
+     * @param view
+     */
     public void onClick(View view) {
-        //
-    }
+        String tag = (String) view.getTag();
 
+        if (TAG_WEB.equals(tag)) {
+            Intent intent = new Intent("android.intent.action.VIEW", Uri.parse("https://www.yahoo.co.jp"));
+            startActivity(intent);
+        }
+    }
 }
