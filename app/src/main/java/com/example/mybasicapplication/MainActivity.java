fe91788164ec1aee7035002b424d49a4153e5ccd
FuminoriArimoto <arimoto@queserser.co.jp>
2019-12-31 16:21:07 +0900

    ボタンレイアウトの勉強。

--- app/src/main/java/com/example/mybasicapplication/MainActivity.java
+++ app/src/main/java/com/example/mybasicapplication/MainActivity.java
@@ -2,16 +2,46 @@ package com.example.mybasicapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private final static  int WC = LinearLayout.LayoutParams.WRAP_CONTENT;
+    private final static String TAG_MESSAGE = "0";
+    private final static String TAG_YESNO = "1";
+    private final static String TAG_TEXT = "2";
+    private final static String TAG_IMAGE = "3";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_main);
+
+        LinearLayout layout = new LinearLayout(this);
+        layout.setBackgroundColor(Color.BLUE);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        setContentView(layout);
+
+        layout.addView(makeButton("メッセージダイアログの表示", TAG_MESSAGE));
+        layout.addView(makeButton("YesNoダイアログの表示", TAG_YESNO));
+        layout.addView(makeButton("テキスト入力ダイアログの表示", TAG_TEXT));
+
+
+    }
+
+    private Button makeButton(String text, String tag) {
+        Button button = new Button(this);
+        button.setText(text);
+        button.setTag(tag);
+        button.setOnClickListener(this);
+        button.setLayoutParams(new LinearLayout.LayoutParams(WC, WC));
+        return button;
     }
 
     /**
@@ -21,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
         TextView tv = (TextView) findViewById(R.id.chage);
         tv.setText("ありまろ！！");
 
+    }
+
+    public void onClick(View view) {
+        //
     }
 
 }
