3c4f4ff03995523c7b49b8dcfaf87a0c5f5ab34f
FuminoriArimoto <arimoto@queserser.co.jp>
2020-01-09 22:40:31 +0900

    singlechoiseを実装した。

--- app/src/main/java/com/example/mybasicapplication/MainActivity.java
+++ app/src/main/java/com/example/mybasicapplication/MainActivity.java
@@ -8,12 +8,19 @@ import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Intent;
 import android.graphics.Color;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,15 +34,48 @@ public class MainActivity extends AppCompatActivity {
     private final static String TAG_OLL = "6";
     private TextView textView;
     private final static int REQUEST_TEXT = 0;
+    private MediaPlayer mediaPlayer;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final ArrayList<String> data = new ArrayList<>();
+        data.add("トルクフレックス");
+        data.add("パワースペリア");
+        data.add("テクニカルパフォーマー");
+        data.add("エアロブースター");
+        data.add("グラハント");
+        data.add("ディープマニピュレーター");
+        data.add("マルチアーマメント");
+        data.add("フェザーストローク");
+        data.add("マグナムドライバー");
+        data.add("スイッチバック");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice, data);
+        ListView list = findViewById(R.id.list);
+        list.setAdapter(adapter);
 
+        list.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        CharSequence msg = ((TextView) view).getText();
+                        Toast.makeText(MainActivity.this, String.format("選択したのは%s", msg.toString()), Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+        this.playSound();
     }
 
+    private void playSound() {
+        mediaPlayer = MediaPlayer.create(this, R.raw.cat1a);
 
+        mediaPlayer.setLooping(false);
+        mediaPlayer.seekTo(0);
+        mediaPlayer.start();
+    }
 }
